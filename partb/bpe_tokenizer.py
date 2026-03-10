aaec35048c81bcedf94b09067ff32c359f3b5468
Laksh Goel <lakshgoel541@gmail.com>
2026-03-11 00:06:14 +0530

    encode split

--- partb/bpe_tokenizer.py
+++ partb/bpe_tokenizer.py
@@ -3,10 +3,19 @@ class BPETokenizer:
         # raise NotImplementedError("BPETokenizer initialization not implemented yet.")
         self.char_to_int = {}
         self.int_to_char = {}
+
+        # Ordered
+        self.merges = []
+
         self.vocab_size = vocab_size
         self.special_tokens = special_tokens
+
         self.vocab = []
+
+        # tuple of chars to int
         self.frequency = {}
+
+        self.UNK_token = "<|UNK|>"
 
     def train(self, corpus):
         raise NotImplementedError("Training method not implemented yet.")
@@ -22,9 +31,23 @@ class BPETokenizer:
 
         # ------ add special tokens (later as they must not be split) ------- assign reserved token IDs
     def encode(self, text):
-        raise NotImplementedError("Encoding method not implemented yet.")
+        # raise NotImplementedError("Encoding method not implemented yet.")
         # -------- text processing --------
         # Treat space character as a distinct token
+        words = text.split()
+
+        segmented = []
+
+        for word in words:
+            chars = []
+            for i, c in enumerate(word):
+                token = (SPACE + c) if i==0 else c
+                if token in self.char_to_int:
+                    chars.append(token)
+                else:
+                    chars.append(self.UNK_token)
+
+            segmented.append(chars) # segmented is list of lists
 
         # --------- Apply bpe merges ---------
         # Iteratively apply merge operations in exact same order
